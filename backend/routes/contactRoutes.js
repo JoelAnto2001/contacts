5f222e769805697097d391dfddbbe1c804f0fcbe
joelanto2001 <joelanto2001@gmail.com>
2024-08-16 14:11:38 +0530

    Routes and controllers

---
+++ backend/routes/contactRoutes.js
@@ -0,0 +1,11 @@
+// contactRoutes.js
+const express = require('express');
+const { getContacts, createContact, updateContact, deleteContact } = require('../controllers/contactController');
+const router = express.Router();
+
+router.get('/', getContacts);
+router.post('/', createContact);
+router.put('/:id', updateContact);
+router.delete('/:id', deleteContact);
+
+module.exports = router;
